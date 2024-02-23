4fa1d3844090ed3e6a4315bad6903496f0b62d29
suvikristiin <suvi.vehmaanpera@gmail.com>
2024-02-23 09:50:16 +0200

    add routes login and registration pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
-
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import MainPage from './components/MainPage.js';
+import LoginPage from './components/LoginPage.js';
+import RegisterPage from './components/RegisterPage.js';
 
 const App = () => {
   return (
     <>
-      <MainPage/>
+      <Router>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+        </Routes>
+      </Router>
     </>
   );
 };
