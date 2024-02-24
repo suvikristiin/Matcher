6da7dfbc36e2700b4a3b2d49788e988bc5858b28
suvikristiin <suvi.vehmaanpera@gmail.com>
2024-02-24 19:25:12 +0200

    add login and registration pages ui

--- client/src/components/LoginPage.js
+++ client/src/components/LoginPage.js
@@ -1,3 +1,45 @@
-const LoginPage = () => {};
+import { Grid, TextField, Button, Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
+import { styled } from '@mui/material/styles';
+import '../styles/LoginPage.css';
+
+const LoginPage = () => {
+  const CustomTextField = styled(TextField)({
+    '& label.Mui-focused': {
+      color: '#fb4444',
+    },
+    '& .MuiOutlinedInput-root': {
+      '&.Mui-focused fieldset': {
+        borderColor: '#fdc7c7',
+      },
+    },
+  });
+
+  return (
+    <Grid container spacing={2}>
+      <Grid id="loginBackground" item>
+        <p id="loginLogo">Matcher</p>
+      </Grid>
+      <Grid item id="loginFormGrid">
+        <form id="loginForm">
+          <Typography id="loginTitle">Log in to the <span>Matcher...</span></Typography>
+          <CustomTextField id="username" label="Username" margin="normal" ></CustomTextField>
+
+          <CustomTextField id="password" label="Password" margin="normal"></CustomTextField>
+          <Button id="loginButton" variant="contained" size="large">
+            Log in
+          </Button>
+          <Typography id="signupLinkInfo">
+            {"Don't have an account?"}
+            <Link id="signupLink" to="/register" style={{ textDecoration: 'none' }}>
+              {' '}
+              Sign Up
+            </Link>
+          </Typography>
+        </form>
+      </Grid>
+    </Grid>
+  );
+};
 
 export default LoginPage;
--- client/src/components/RegisterPage.js
+++ client/src/components/RegisterPage.js
@@ -1,3 +1,50 @@
-const RegisterPage = () => {};
+import { Grid, TextField, Button, Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
+import { styled } from '@mui/material/styles';
+import '../styles/RegisterPage.css';
+
+const RegisterPage = () => {
+  const CustomTextField = styled(TextField)({
+    '& label.Mui-focused': {
+      color: '#fb4444',
+    },
+    '& .MuiOutlinedInput-root': {
+      '&.Mui-focused fieldset': {
+        borderColor: '#fdc7c7',
+      },
+    },
+  });
+
+  return (
+    <Grid container spacing={2}>
+      <Grid id="registerBackground" item>
+        <p id="registerLogo">Matcher</p>
+      </Grid>
+      <Grid item id="registerFormGrid">
+        <form id="registerForm">
+          <Typography id="signupTitle">
+            Sign up for the <span>Matcher...</span>
+          </Typography>
+          <CustomTextField id="email" label="Email" margin="normal"></CustomTextField>
+
+          <CustomTextField id="username" label="Username" margin="normal"></CustomTextField>
+
+          <CustomTextField id="password" label="Password" margin="normal"></CustomTextField>
+
+          <Button id="signupButton" variant="contained" size="large">
+            Sign Up
+          </Button>
+          <Typography id="loginLinkInfo">
+            Do you already have an account?
+            <Link id="loginLink" to="/login" style={{ textDecoration: 'none' }}>
+              {' '}
+              Log In
+            </Link>
+          </Typography>
+        </form>
+      </Grid>
+    </Grid>
+  );
+};
 
 export default RegisterPage;
